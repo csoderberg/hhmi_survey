009de70659cb396b4b8719c9fa06e1adb6b9479a
csoderberg <csoderbe@gmail.com>
2020-02-11 16:19:03 -0500

    remove unneeded stacked bar graph

--- figures.R
+++ figures.R
@@ -67,32 +67,3 @@ plot <- likert(as.data.frame(character_data$funder_mandate), grouping = fct_rev(
 
 
 ggsave('test_plot.jpeg', plot, dpi = 600, height = 8, width = 24, units = 'in')
-
-
-
-
-## example graph stacked bar with percentages 
-character_data %>%
-  select(funder_names, funder_mandate) %>%
-  filter(funder_names != 'Hanna Gray Fellows') %>%
-  filter(!is.na(funder_mandate)) %>%
-  group_by(funder_names, funder_mandate) %>%
-  tally() %>%
-  mutate(perc = round(100*n/sum(n),0),
-         percentage = paste0(perc, '%')) %>%
-  ggplot(aes(fill = fct_rev(funder_mandate), x = funder_names, y = perc)) +
-  geom_col(stat = 'identity', position = 'fill', width = .5) +
-  geom_text(aes(x = funder_names ,label = percentage), size = 4.938889, position=position_fill(vjust=0.5)) + ## 14 * 0.352777778 since this geom_text size is in mm, not pt like microsoft and theme
-  scale_y_continuous(labels=scales::percent, expand = c(0, 0)) +
-  scale_fill_manual(values=c('#058d96', '#00a450','#8ac341','#AAAAAA','#838286')) +
-  theme(axis.text = element_text(family = 'Helvetica', size = 14),
-        axis.line = element_line(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        panel.background = element_rect(fill = "white", colour = "white"),
-        legend.position = 'bottom',
-        legend.text = element_text(family = 'Helvetica', size = 14,  margin = margin(r = 75, unit = "pt")),
-        legend.title = element_blank(),
-        plot.margin = margin(t = 10, l = 5.5, r = 5.5, b = 10, "pt")) +
-  coord_flip() +
-  guides(fill = guide_legend(reverse = TRUE))
